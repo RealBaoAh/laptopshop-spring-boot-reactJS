e0462b3e9b92bcc5d65fbf801f99a9382da86176
RealBaoAh <baoanh2003ln@gmail.com>
2026-05-18 21:44:50 +0700

    spring mvc set up

---
+++ src/main/java/vn/baoah/laptopshop/controller/UserController.java
@@ -0,0 +1,31 @@
+package vn.baoah.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.baoah.laptopshop.service.UserService;
+
+// @Controller
+// public class UserController {
+//     @RequestMapping("/")
+//     public String getHomePage(){
+//         return "Home Page";
+//     }
+// }
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+    
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public String getHomePage(){
+        return this.userService.handleHello();
+    }
+}
--- src/main/java/vn/baoah/laptopshop/services/MyService.java
+++
@@ -1,8 +0,0 @@
-package vn.baoah.laptopshop.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class MyService {
-    
-}
