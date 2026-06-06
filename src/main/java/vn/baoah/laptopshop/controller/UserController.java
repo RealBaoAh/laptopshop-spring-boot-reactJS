06c86b1a6dc1215ad8c6276db4b6017bfb6e2478
RealBaoAh <baoanh2003ln@gmail.com>
2026-06-06 12:21:15 +0700

    create  user form

--- src/main/java/vn/baoah/laptopshop/controller/UserController.java
+++ src/main/java/vn/baoah/laptopshop/controller/UserController.java
@@ -2,10 +2,11 @@ package vn.baoah.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.baoah.laptopshop.domain.User;
 import vn.baoah.laptopshop.service.UserService;
 
 @Controller
@@ -24,19 +25,18 @@ public class UserController {
         model.addAttribute("baoanh1", "this is bao anh");
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model){
+        String test = this.userService.handleHello();
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User baoanh){
+        System.out.println("run here " +  baoanh);
+        return "hello";
+    }
 }
 
-// @RestController
-// public class UserController {
-
-//     private UserService userService;
-    
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
-
-//     @GetMapping("")
-//     public String getHomePage(){
-//         return this.userService.handleHello();
-//     }
-// }
